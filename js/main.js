0213e47b77240725b01fe5d792a6bfc82b3f9552
ellijayne <ellijayne.m.willard@gmail.com>
2018-08-22 20:29:44 +1000

    took out fixed nav cos it kept screwing with responsivness

--- js/main.js
+++ js/main.js
@@ -1,36 +1,56 @@
 console.log('are you there?');
 
-// $(document).ready(function() {
-
 
 //smooth scroll function
-// $('a[href^="#"]').on('click',function (e) {
-//     // e.preventDefault();
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    let target = this.hash,
+    $target = $(target);
+    let newTop = $('.page-container').scrollTop() + $target.position().top-70px;
+   $('.page-container').stop().animate({
+     'scrollTop': newTop
+   }, 1100, 'linear', function () {
+     // window.location.hash = target;
+    });
+});
+// //
+// $('#home-btn').on('click', function(e) {
+//   e.preventDefault();
+//   $('.page-container').animate({
+//     scrollTop: 0
+//     }, 1100);
+//   });
 //
-//     let target = this.hash,
-//     $target = $(target);
-//
-//    $('html, body').stop().animate({
-//      'scrollTop': $target.offset().top-70
-//     }, 900, 'swing', function () {
-//      window.location.hash = target;
-//     });
+// $("#aboutme-btn").on('click', function(e) {
+//     e.preventDefault();
+//     console.log(e);
+//       $('.page-container').animate({
+//           scrollTop: $("#aboutme-section").offset().top - 70
+//       }, 1100);
 // });
 //
-$('#home-btn').on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({
-    scrollTop: 0
-    }, 1100);
-  });
-
-$("#aboutme-btn").on('click', function(e) {
-    e.preventDefault();
-    console.log(e);
-      $('html, body').animate({
-          scrollTop: $("#aboutme-section").offset().top - 70
-      }, 1100);
-});
+// $("#portfolio-btn").on('click', function(e) {
+//     e.preventDefault();
+//       $('.page-container').animate({
+//           scrollTop: $("#portfolio-section").offset().top - 70
+//       }, 1100);
+// });
+// $("#contact-btn").on('click', function(e) {
+//     e.preventDefault();
+//       $('.page-container').animate({
+//           scrollTop: $("#contact-section").offset().top - 70
+//       }, 1100);
+// });
 
 
-// })
+//TRYING TO FIX MENU
+//
+// $('.page-container').bind('scroll', function () {
+//     var nav = $('nav');
+//     if ($('.page-container').scrollTop() > nav.offset().top) {
+//         nav.addClass('fixed');
+//     } else {
+//         nav.removeClass('fixed');
+//     }
+// });
