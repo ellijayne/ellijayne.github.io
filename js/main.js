ac0c811b5c3dcdfdea1010562e901cfd6e4059fc
ellijayne <ellijayne.m.willard@gmail.com>
2018-08-24 18:53:30 +1000

    added conditional if statement to jquery re screen width and smooth scroll

--- js/main.js
+++ js/main.js
@@ -7,10 +7,19 @@ $('a[href^="#"]').on('click',function (e) {
 
     let target = this.hash,
     $target = $(target);
-    let newTop = $('html body').scrollTop() + $target.position().top-160;
-   $('html, body').stop().animate({
-     'scrollTop': newTop
-   }, 1100, 'linear', function () {
-
-    });
+    if ($(window).width() > 450) {
+      // console.log('window is OVER 450px');
+      let newTop = $('html body').scrollTop() + $target.position().top-160;
+      $('html, body').stop().animate({
+        'scrollTop': newTop
+      }, 1100, 'linear', function () {
+      });
+    } else {
+      // console.log('window is UNDER 450px');
+      let newTop = $('#page-container').scrollTop() + $target.position().top-160;
+      $('#page-container').stop().animate({
+        'scrollTop': newTop
+      }, 1100, 'linear', function () {
+      });
+    }
 });
