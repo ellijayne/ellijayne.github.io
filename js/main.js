c7fe5fe8173d9261b75a3b647f6ad492b65e2dfc
Elli-Jayne (EJ) Willard <ej.willard@jaxsta.com>
2021-08-27 16:09:56 +1000

    removing js scroll and just using css

--- js/main.js
+++ js/main.js
@@ -4,25 +4,25 @@ $(document).ready(function() {
   console.log('are you there?');
 
   //smooth scroll function
-  $('a[href^="#"]').on('click',function (e) {
-    e.preventDefault();
+  // $('a[href^="#"]').on('click',function (e) {
+  //   e.preventDefault();
 
-    let target = this.hash,
-    $target = $(target);
-    if ($(window).width() > 500) {
-      let newTop = $('html body').scrollTop() + $target.position().top-160;
-      $('html, body').stop().animate({
-        'scrollTop': newTop
-      }, 1100, 'linear', function () {
-      });
-    } else {
-      let newTop = $('#page-container').scrollTop() + $target.position().top-160;
-      $('#page-container').stop().animate({
-        'scrollTop': newTop
-      }, 1100, 'linear', function () {
-      });
-    }
-  });
+  //   let target = this.hash,
+  //   $target = $(target);
+  //   if ($(window).width() > 500) {
+  //     let newTop = $('html body').scrollTop() + $target.position().top-160;
+  //     $('html, body').stop().animate({
+  //       'scrollTop': newTop
+  //     }, 1100, 'linear', function () {
+  //     });
+  //   } else {
+  //     let newTop = $('#page-container').scrollTop() + $target.position().top-160;
+  //     $('#page-container').stop().animate({
+  //       'scrollTop': newTop
+  //     }, 1100, 'linear', function () {
+  //     });
+  //   }
+  // });
 
 
   //read more dropdown
